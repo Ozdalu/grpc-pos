5dafba7104a0ffb0d9ea0cf1b839c8dd4e12866c
Ozdalu <pbisset@protonmail.com>
2024-07-19 14:56:45 +0200

    refacto: put actions in their own functions

--- client/main.go
+++ client/main.go
@@ -21,48 +21,70 @@ func main() {
 	ctx, CtxCancel := context.WithTimeout(context.Background(), 30 * time.Second)
 	defer CtxCancel()
 
-  /* //// REGISTER
+  // uuid := register(grpc_client, ctx)
+  const uuid string = "4230130246536364507"
+
+  subscribe(uuid, grpc_client, ctx)
+  // getLastBlock(grpc_client, ctx)
+  addTransaction(uuid, "7354821206227837404", 1, "on vit dans une #société........... j'espère ne pas me faire #slash cette fois.......", grpc_client, ctx)
+  // bakeBlock(uuid, grpc_client, ctx)
+  // confirmBake(uuid, grpc_client, ctx)
+}
+
+func register(grpc_client protobuf.BlockchainClient, ctx context.Context) string {
 	registerResponse, err := grpc_client.Register(ctx, &protobuf.Empty{})
 	if err != nil {
 		log.Fatalf("Register Error: %v", err)
 	}
+
   log.Printf("\nUuid : %v\nReputation : %d", registerResponse.GetUuid(), registerResponse.GetReputation())
-  uuid := registerResponse.GetUuid() */
+  return registerResponse.GetUuid()
+}
 
-  const uuid string = "4827309344320181817"
-
-  //// SUBSCRIBE
+func subscribe(uuid string, grpc_client protobuf.BlockchainClient, ctx context.Context) string {
 	subscribeResponse, err := grpc_client.Subscribe(ctx, &protobuf.SubscribeRequest{Uuid: uuid})
 	if err != nil {
 		log.Fatalf("Subscribe Error: %v", err)
 	}
   log.Printf("\nMessage : %v", subscribeResponse.GetMessage())
 
-  //// GETLASTBLOCK
+  return subscribeResponse.GetMessage()
+}
+
+func getLastBlock(grpc_client protobuf.BlockchainClient, ctx context.Context) protobuf.BlockInfo {
 	getLastBlockResponse, err := grpc_client.GetLastBlock(ctx, &protobuf.Empty{})
 	if err != nil {
 		log.Fatalf("GetLastBlock Error: %v", err)
 	}
   log.Printf("\nBlockInfo :\n\tHash : %v\n\tBlock Number : %d\n\tPrevious Block Hash : %v", getLastBlockResponse.GetBlockHash(), getLastBlockResponse.GetBlockNumber(), getLastBlockResponse.GetPreviousBlockHash())
 
-  //// ADD TRANSACTION
-  addTransactionResponse, err := grpc_client.AddTransaction(ctx, &protobuf.Transaction{Sender: uuid, Receiver: "6653957458531705520", Amount: 0, Data: "Askip je bake tous les blocs, marre d'être le seul :("})
+  return protobuf.BlockInfo{}
+}
+
+func addTransaction(sender string, receiver string, amount int32, data string, grpc_client protobuf.BlockchainClient, ctx context.Context) {
+  _, err := grpc_client.AddTransaction(ctx, &protobuf.Transaction{Sender: sender, Receiver: receiver, Amount: amount, Data: data })
 	if err != nil {
 		log.Fatalf("AddTransaction Error: %v", err)
 	}
-  log.Printf("\nSuccess sending transaction %v", addTransactionResponse)
 
-  //// BAKE BLOCK
+  log.Printf("\nSuccess sending transaction")
+}
+
+func bakeBlock(uuid string, grpc_client protobuf.BlockchainClient, ctx context.Context) protobuf.BakeResponse {
   bakeBlockResponse, err := grpc_client.BakeBlock(ctx, &protobuf.BakeRequest{Uuid: uuid})
 	if err != nil {
 		log.Fatalf("BakeBlock Error: %v", err)
 	}
   log.Printf("\nUuid : %v\nMessage : %v", bakeBlockResponse.GetUuid(), bakeBlockResponse.GetMessage())
 
-  //// CONFIRMBAKE
-	confirmBakeResponse, err := grpc_client.ConfirmBake(ctx, &protobuf.ConfirmRequest{Uuid: uuid})
+  return protobuf.BakeResponse{}
+}
+
+func confirmBake(uuid string, grpc_client protobuf.BlockchainClient, ctx context.Context) {
+	_, err := grpc_client.ConfirmBake(ctx, &protobuf.ConfirmRequest{Uuid: uuid})
 	if err != nil {
-		log.Fatalf("ConfirmBlake Error: %v", err)
+		log.Fatalf("ConfirmBake Error: %v", err)
 	}
-  log.Printf("Confirm Blake success ! %v", confirmBakeResponse)
+
+  log.Printf("Confirm Bake success !")
 }
