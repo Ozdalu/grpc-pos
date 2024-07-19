2a385cd4067ceb8672c8a3104b0e5faaf8310cc4
Ozdalu <pbisset@protonmail.com>
2024-07-19 11:57:26 +0200

    feat: server created from teacher's

--- server/main.go
+++ server/main.go
@@ -1,36 +1,48 @@
 package main
 
 import (
-	"context"
-	"log"
-	"net"
+    "context"
+    "fmt"
+    "log"
+    "math/rand"
+    "net"
 
-	protobuf "github.com/ozdalu/grpc-pos/proto"
-	"google.golang.org/grpc"
+    pb "github.com/ozdalu/grpc-pos/proto"
+    "google.golang.org/grpc"
 )
 
-/* type HelloServiceServer struct {
-	protobuf.UnimplementedHelloServiceServer
-} */
-
-/* func (s *HelloServiceServer) SayHello(ctx context.Context, in *protobuf.InputRequest) (*protobuf.OutputRequest, error) {
-	log.Printf("Received: %v", in.GetName())
-	return &protobuf.OutputRequest{Message: "Hello " + in.GetName()}, nil
-} */
-
-func main() {
-  log.Printf("Hello World")
-
-	/* listener, err := net.Listen("tcp", "localhost:8080")
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-
-	grpc_server := grpc.NewServer()
-	protobuf.RegisterHelloServiceServer(grpc_server, &HelloServiceServer{})
-	log.Printf("server listening at %v", listener.Addr())
-
-	if err := grpc_server.Serve(listener); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	} */
+type Block struct {
 }
+
+type server struct {
+    pb.UnimplementedBlockchainServer
+}
+
+func (s *server) Register(ctx context.Context, in *pb.Empty) (*pb.RegisterResponse, error) {
+    uuid := generateUUID()
+    reputation := int32(100)
+    log.Printf("Client %s registered with reputation %d", uuid, reputation)
+    return &pb.RegisterResponse{Uuid: uuid, Reputation: reputation}, nil
+}
+
+func generateUUID() string {
+    return fmt.Sprintf("%d", rand.Int63())
+}
+
+func main() {    
+    lis, err := net.Listen("tcp", ":50051")
+    if err != nil {
+        log.Fatalf("failed to listen: %v", err)
+    }
+    log.Println("Server listening on port 50051")
+    
+    s := grpc.NewServer()
+    
+    pb.RegisterBlockchainServer(s, &server{})
+    log.Println("Server started successfully")
+
+    if err := s.Serve(lis); err != nil {
+        log.Fatalf("failed to serve: %v", err)
+    }
+}
+
